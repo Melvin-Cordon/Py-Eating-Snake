633ad14845918188f53f7255b1735560e1b779ab
Melvin-Cordon <melvin.cordon21@gmail.com>
2020-08-04 22:49:41 -0500

    added function to respond to key presses

--- game_functions.py
+++ game_functions.py
@@ -13,3 +13,34 @@ def update_screen(settings,screen,snake):
 
     # Make the most recently drawn screen visible
     pg.display.flip()
+
+def check_event(settings, screen, snake):
+    """Respond to keypresses and mouse events """
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit()
+        elif event.type == pg.KEYDOWN:
+            check_keydown_events(event,settings, screen, snake)
+        elif event.type == pg.KEYUP:
+            check_keyup_events(event, snake)
+
+def check_keydown_events(event,settings,screen,snake):
+    """ Respond to keypresses """
+    if event.key == pg.K_d:
+        print('d')
+    elif event.key == pg.K_a:
+        print('a')
+    elif event.key == pg.K_w:
+        print('w')
+    elif event.key == pg.K_s:
+        print('s')
+
+def check_keyup_events(event,snake):
+    if event.key == pg.K_d:
+        print('d')
+    elif event.key == pg.K_a:
+        print('a')
+    elif event.key == pg.K_w:
+        print('w')
+    elif event.key == pg.K_s:
+        print('s')
